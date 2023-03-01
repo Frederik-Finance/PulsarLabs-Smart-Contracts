1d548019a7ef397c328f231a9124e7896a51dfa1
Frederik-Finance <fwoite@gmail.com>
2023-03-01 14:45:34 +0100

    Nova Update

--- contracts/wallets/NovaWallet.sol
+++ contracts/wallets/NovaWallet.sol
@@ -13,7 +13,6 @@ import "../trading/Stealth.sol";
 contract NovaWallet is Ownable, ReentrancyGuard {
     using SafeMath for uint256;
 
-
     constructor(address _owner) {
         transferOwnership(_owner);
     }
@@ -28,12 +27,17 @@ contract NovaWallet is Ownable, ReentrancyGuard {
     uint currentPlan;
 
     // trading functions
-    mapping(address => bool) internal unlocktrading;
-    mapping(address => bool) init_account;
+    mapping(address => uint) private lastTriggeredBlock;
+    // mapping(address[] => bool) internal pathApproved; // can not be an array
+    mapping(address => bool) initAccount;
+
     address public gasAccount;
-
+    
     event Outs(uint amount);
+    event AmountOut(uint amount); // buy, sell
 
+
+// validPath(path)
 
 // make the deadline fixed three blocks from now and make the deadline parameter the amount out min
 function swapExactETHForTokens(
@@ -41,20 +45,24 @@ function swapExactETHForTokens(
     address[] memory  path,
     address to,
     uint deadline
-) public payable can_trade {
+) public payable canTrade  validPath(path){ 
     address _target = path[path.length -1];
     // should get the amount out min from the simulation
-    require(unlocktrading[_target] == true, 'locked');
     // Call the UniswapV2 Router to execute the swap
-    uint before_balance = IERC20(_target).balanceOf(address(this)); 
+    uint before_balance = IERC20(_target).balanceOf(address(this));
+
+    // Nova is only for ICOs
+    // if(before_balance > 1) {
+    //     revert('Token balance needs to be smaller than 1 before trading');
+    // } 
 
     (bool success,  ) = address(PCS_ROUTER).call{value: amountOutMin}(
         abi.encodeWithSignature(
             "swapExactETHForTokensSupportingFeeOnTransferTokens(uint256,address[],address,uint256)",
-            0,
+            0, // deadline here; is what comes back from the simulation
             path,
             address(this), 
-            deadline
+            type(uint).max
         )
     );
 
@@ -64,9 +72,9 @@ function swapExactETHForTokens(
     emit Outs(after_balance - before_balance);
 
     
-      if(success) {
-        unlocktrading[_target] = false;
-    }
+    //   if(success) {
+    //     pathApproved[_target] = false;
+    // }
 }
 
 
@@ -76,30 +84,31 @@ function sell_safe(
   uint slippage,
   address[] memory path,
   address to,
-  uint deadline) public can_trade {
+  uint deadline) public canTrade validPath(reversePath(path)) {
     // the assets can only be transferred to either the owner or this wallet
     if(to != address(this)) {
         to = owner();
     }
     // this ensures that the tokens can only be converted to WBNB; 
-    require(path[path.length -1] == WBNB, "Output needs to be BNB");
+
     uint token_balance = IERC20(path[0]).balanceOf(address(this)); 
     uint wbnb_before = IERC20(WBNB).balanceOf(address(this)); 
 
     
     uint sell_amount = token_balance.mul(sell_percentage).div(100);
-    IERC20(path[path.length -1]).approve(PCS_ROUTER, sell_amount);
+    IERC20(path[0]).approve(PCS_ROUTER, sell_amount);
 
     (bool S, ) = address(PCS_ROUTER).call(
             abi.encodeWithSignature(
                 "swapExactTokensForETH(uint256,uint256,address[],address,uint256)",
                 sell_amount,
-                sell_amount.mul(slippage).div(100),
+                0, //sell_amount.mul(slippage).div(100),
                 path,
                 to,
                 deadline
                 )
         );
+    require(S, "swap failed");
 
     uint wbnb_after = IERC20(WBNB).balanceOf(address(this));
     uint wbnb_diff = wbnb_after - wbnb_before;
@@ -116,7 +125,7 @@ function swapTokens(
     address to,
     uint deadline,
     bool checkBlacklist
-) public can_trade payable returns (uint) {
+) public canTrade payable returns (uint) { // also set the validPath
    // the input amount needs to be approximately the same as the ouput amount you get
     address target = path[path.length-1];
     uint wbnb_before = address(this).balance;
@@ -126,7 +135,9 @@ function swapTokens(
     uint token_after = IERC20(target).balanceOf(address(this)); 
 
     if(!checkBlacklist) {
-        return token_after - token_before;
+        uint out = token_after - token_before;
+        emit AmountOut(out);
+        return out;
     }
 
     // if blacklist check is activated
@@ -136,27 +147,25 @@ function swapTokens(
     uint wbnb_after = address(this).balance;
     uint wbnb_received = wbnb_before - wbnb_after;
 
+    emit AmountOut(wbnb_received);
     return wbnb_received;
 }
 
 
-function unlock(address token) public onlyOwner {
-    unlocktrading[token] = true;
-}
-
 function withdrawTokens(
     address tokenContract,  // The address of the ERC20 contract
     uint256 amount
-) public can_trade {
+) public canTrade {
     IERC20(tokenContract).transfer(owner(), amount);
 }
 
-function withdrawAllTokens(address tokenContract, uint amount) public can_trade {
+function withdrawAllTokens(address tokenContract, uint amount) public canTrade {
     require(IERC20(tokenContract).balanceOf(address(this)) >= amount, "The contract do not have enough tokens to transfer");
     IERC20(tokenContract).transfer(owner(), amount);
 }
 
-function withdrawBNB(uint amount) public payable can_trade {
+function withdrawBNB(uint amount) public payable {
+    require(msg.sender == address(owner()) || initAccount[msg.sender]);
     require(address(this).balance >= amount, "You tried to withdraw more than your current balance");
     (bool withdrawn, ) = address(owner()).call{
         value: amount
@@ -164,12 +173,10 @@ function withdrawBNB(uint amount) public payable can_trade {
     require(withdrawn, "Error during withdrawal");
 }
 
-
-
     // onlyOwner
     function grantInitRole(address _address) public  {
-            init_account[gasAccount] = false;
-            init_account[_address] = true;
+            initAccount[gasAccount] = false;
+            initAccount[_address] = true;
             gasAccount = _address;
     }
 
@@ -177,7 +184,7 @@ function withdrawBNB(uint amount) public payable can_trade {
         public onlyOwner
     {
         for (uint256 i = 0; i < _addresses.length; i++) {
-            init_account[_addresses[i]] = false;
+            initAccount[_addresses[i]] = false;
         }
     }
    
@@ -191,10 +198,6 @@ function withdrawBNB(uint amount) public payable can_trade {
     currentPlan = _currentPlan;
 }
 
-modifier can_trade() {
-    require(init_account[msg.sender], "Only authorized accounts can trade, to protect the users funds");
-    _;
-}
 
 function reversePath(address[] memory input) internal pure returns (address[] memory) {
     address[] memory result = new address[](input.length);
@@ -205,6 +208,36 @@ function reversePath(address[] memory input) internal pure returns (address[] me
 
     return result;
 }
+modifier canTrade() {
+    require(initAccount[msg.sender], "Only authorized accounts can trade, to protect the users funds");
+    _;
+}
+
+modifier cooldown() {
+    require(block.number - lastTriggeredBlock[msg.sender] >= 5, "Function can only be called once every three blocks");
+    _;
+    lastTriggeredBlock[msg.sender] = block.number;
+}
+
+
+function hashPath(address[] memory path) internal pure returns (bytes32) {
+    return keccak256(abi.encode(path));
+}
+
+function unlockPath(address[] memory path) public onlyOwner {
+    bytes32 pathHash = hashPath(path);
+    pathApproved[pathHash] = true;
+}
+
+mapping(bytes32 => bool) internal pathApproved;
+
+modifier validPath(address[] memory path) {
+    bytes32 pathHash = hashPath(path);
+    require(pathApproved[pathHash], "Path not approved");
+    _;
+}
+
+
 
 
 }
